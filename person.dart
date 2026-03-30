696559be3b706784eb98ef7fbe25ead123315e44
kill-ux <mustaphaboutoubdev@gmail.com>
2026-03-30 16:07:46 +0100

    person.dart

---
+++ person.dart
@@ -0,0 +1,15 @@
+
+
+class Person {
+	String name;
+	String surname;
+	String cityOfOrigin;
+	int age;
+	int? height;
+
+	Person(this.name,this.surname,this.cityOfOrigin, this.age, [this.height]);
+
+}
+
+
+
