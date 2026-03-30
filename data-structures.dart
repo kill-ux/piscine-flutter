7eccad6c85093824ed1c406dd34d099b6b2ba48e
kill-ux <mustaphaboutoubdev@gmail.com>
2026-03-30 11:39:21 +0100

    data-structures.dart

---
+++ data-structures.dart
@@ -0,0 +1,13 @@
+
+var listNum = [0, 0, 0, 0, 0];
+var listObj = [0, 3.14, "str", true];
+var listStr = ["list","of","strings"];
+var listList = [listNum, listObj, listStr];
+var set = {'Japan', 'Tokyo', 'Shiboya'};
+var mapStr = {
+	2025: "last",
+	2026: "current",
+	2027: 'next'
+}; 
+
+
--- variables.dart
+++ variables.dart
@@ -3,4 +3,4 @@ Object obj = "any value except Null";
 var planet = "planet";
 var year = 2026;
 var lucky = true;
-var pi = 3.14; 
+var pi = 3.14;
