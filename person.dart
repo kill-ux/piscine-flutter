35c62ee8f9af9e61c669672e798fd2360fc31733
kill-ux <mustaphaboutoubdev@gmail.com>
2026-03-31 10:24:05 +0100

    fix peson

--- person.dart
+++ person.dart
@@ -1,15 +1,10 @@
-
-
 class Person {
-	String name;
-	String surname;
-	String cityOfOrigin;
-	int age;
-	int? height;
+  String name;
+  String? surname;
+  String cityOfOrigin;
+  int age;
+  int? height;
 
-	Person(this.name,this.surname,this.cityOfOrigin, this.age, [this.height]);
-
+  Person(this.name, this.cityOfOrigin, this.age, this.height, [this.surname]);
 }
-
-
-
+  
